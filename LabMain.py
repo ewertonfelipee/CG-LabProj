c5a74e7a08c3a73df14778403fcdb5f94b7f1a7b
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2023-05-18 10:20:01 -0300

    add chair 2 and 3

--- LabMain.py
+++ LabMain.py
@@ -353,7 +353,7 @@ def draw_lamp():
 def draw_lamp2():
     # base da luminária
     glPushMatrix()
-    glTranslatef(6.2, -1.6, 5)
+    glTranslatef(5.5, -1.6, 5)
     glScalef(0.8, 0.2, 0.8)
     glColor3f(0.5, 0.5, 0.5)
     glutSolidCube(1.0)
@@ -361,7 +361,7 @@ def draw_lamp2():
 
     # haste da luminária
     glPushMatrix()
-    glTranslatef(6.2, -1, 5)
+    glTranslatef(5.5, -1, 5)
     glScalef(0.1, 1.5, 0.1)
     glColor3f(0.5, 0.5, 0.5)
     glutSolidCube(1.0)
@@ -369,7 +369,7 @@ def draw_lamp2():
 
     # cabeça da luminária
     glPushMatrix()
-    glTranslatef(6.2, 0.0, 5)
+    glTranslatef(5.5, 0.0, 5)
     glScalef(0.8, 0.5, 0.8)
     glColor3f(1.0, 1.0, 1.0)  # amarelo
     glutSolidCube(1.0)
@@ -406,7 +406,7 @@ def draw_notebook(texture_notebook):
     glBindTexture(GL_TEXTURE_2D, texture_notebook)
 
     glPushMatrix()
-    glTranslatef(0.0, -1.6, 5)
+    glTranslatef(-1.0, -1.6, 5)
     glScale(1.0,0.0,1.0)
     glColor3f(0.5, 0.5, 0.5)
 
@@ -433,7 +433,7 @@ def draw_notebook2(texture_notebook2):
     glBindTexture(GL_TEXTURE_2D, texture_notebook2)
 
     glPushMatrix()
-    glTranslatef(4, -1.6, 5)
+    glTranslatef(3.5, -1.6, 5)
     glScale(1.0,0.0,1.0)
     glColor3f(0.5, 0.5, 0.5)
 
@@ -513,7 +513,145 @@ def draw_chair():
     glPushMatrix()
 
     # mover a cadeira para a posição desejada
-    glTranslatef(0.0, -1.5, 3.0)
+    glTranslatef(-1.0, -1.5, 3.0)
+
+    # definir a escala da cadeira
+    glScalef(3.0, 2.0, 2.0)
+
+    # definir a cor da cadeira
+    glColor3f(0.0, 0.0, 0.0)
+
+    # desenhar o assento da cadeira
+    glPushMatrix()
+    glTranslatef(0.0, -0.68, 0.0)
+    glScalef(0.8, 0.1, 0.8)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    #desenha a junção esquerda entre o assento o encosto
+    glPushMatrix()
+    glTranslatef(-0.3, -0.2, -0.3)
+    glScalef(0.1, 3.0, 0.1)
+    glColor3f(0.0, 0.0, 0.0)
+    glutSolidCube(0.3)
+    glPopMatrix()
+
+    #desenha a junção direita entre o assento o encosto
+    glPushMatrix()
+    glTranslatef(0.3, -0.2, -0.3)
+    glScalef(0.1, 3.0, 0.1)
+    glColor3f(0.0, 0.0, 0.0)
+    glutSolidCube(0.3)
+    glPopMatrix()
+
+    # desenhar os pés da cadeira
+    glPushMatrix()
+    glTranslatef(-0.3, -1.2, 0.3)
+    glScalef(0.1,1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPushMatrix()
+    glTranslatef(-0.3, -1.2, -0.3)
+    glScalef(0.1, 1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPushMatrix()
+    glTranslatef(0.3, -1.2, 0.3)
+    glScalef(0.1, 1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPushMatrix()
+    glTranslatef(0.3, -1.2, -0.3)
+    glScalef(0.1, 1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    # desenhar o encosto da cadeira
+    glPushMatrix()
+    glTranslatef(0.0, 0.2, -0.3)
+    glScalef(0.8, 0.5, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPopMatrix()
+
+def draw_chair2():
+    glPushMatrix()
+
+    # mover a cadeira para a posição desejada
+    glTranslatef(3.5, -1.5, 3.0)
+
+    # definir a escala da cadeira
+    glScalef(3.0, 2.0, 2.0)
+
+    # definir a cor da cadeira
+    glColor3f(0.0, 0.0, 0.0)
+
+    # desenhar o assento da cadeira
+    glPushMatrix()
+    glTranslatef(0.0, -0.68, 0.0)
+    glScalef(0.8, 0.1, 0.8)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    #desenha a junção esquerda entre o assento o encosto
+    glPushMatrix()
+    glTranslatef(-0.3, -0.2, -0.3)
+    glScalef(0.1, 3.0, 0.1)
+    glColor3f(0.0, 0.0, 0.0)
+    glutSolidCube(0.3)
+    glPopMatrix()
+
+    #desenha a junção direita entre o assento o encosto
+    glPushMatrix()
+    glTranslatef(0.3, -0.2, -0.3)
+    glScalef(0.1, 3.0, 0.1)
+    glColor3f(0.0, 0.0, 0.0)
+    glutSolidCube(0.3)
+    glPopMatrix()
+
+    # desenhar os pés da cadeira
+    glPushMatrix()
+    glTranslatef(-0.3, -1.2, 0.3)
+    glScalef(0.1,1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPushMatrix()
+    glTranslatef(-0.3, -1.2, -0.3)
+    glScalef(0.1, 1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPushMatrix()
+    glTranslatef(0.3, -1.2, 0.3)
+    glScalef(0.1, 1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPushMatrix()
+    glTranslatef(0.3, -1.2, -0.3)
+    glScalef(0.1, 1, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    # desenhar o encosto da cadeira
+    glPushMatrix()
+    glTranslatef(0.0, 0.2, -0.3)
+    glScalef(0.8, 0.5, 0.1)
+    glutSolidCube(1.0)
+    glPopMatrix()
+
+    glPopMatrix()
+
+def draw_chair3():
+    glPushMatrix()
+
+    # mover a cadeira para a posição desejada
+    glTranslatef(7.0, -1.5, 3.0)
 
     # definir a escala da cadeira
     glScalef(3.0, 2.0, 2.0)
@@ -676,6 +814,8 @@ def draws():
     draw_board(textures["board"])
     draw_sphere()
     draw_chair()
+    draw_chair2()
+    draw_chair3()
     draw_cabinet()
     draw_lamp()
     draw_lamp2()
